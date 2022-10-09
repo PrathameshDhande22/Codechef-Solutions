05fd00a9b7a13391368a680bd64a882bc6ce7ce4
Prathamesh Dhande <prathameshdhande5139@gmail.com>
2022-10-09 11:22:52 +0530

    OCT221D Problem

---
+++ contest/SINGLEOP1.java
@@ -0,0 +1,27 @@
+package codechef.contest;
+
+import java.util.Scanner;
+
+public class SINGLEOP1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cases = sc.nextInt();
+		for (int i = 0; i < cases; i++) {
+			int len = sc.nextInt();
+			String binary = sc.next();
+			int count = 0;
+			for (int a = 0; a < binary.length(); a++) {
+				char bin = binary.charAt(a);
+				if (bin == '1') {
+					count++;
+				} else {
+					break;
+				}
+			}
+			System.out.println(count);
+		}
+		sc.close();
+	}
+
+}
